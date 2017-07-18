196ff7f5190b6719b2a1212c2d7553ab7948a703
Neil Steiner <npsteiner@gmail.com>
2017-07-18 11:55:50 -0400

    Worked on lab together on one computer

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,30 +2,69 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+# def get_info_for_film(film_url)
+  
+# end
+
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+
+  #puts character_hash["results"]
+
+  # character_hash is an array of hashes (one hash per char)
   
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+  film_link_array = []
+  character_hash["results"].each do |each_character|
+    if each_character["name"].downcase.include?(character)
+      film_link_array = each_character["films"]
+      puts "You searched for: #{each_character["name"]}"
+    end
+  end
+
+
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+  film_link_array.map do |film_link|
+    #puts film_link
+    film_info = RestClient.get(film_link)
+    film_info_hash = JSON.parse(film_info)
+  end
+   # film_link_array is array of film_info_hashes, one for each film character is in
+
+
+
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
+
+
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
 end
 
+
+
+
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index do |film, index|
+    puts "#{index+1} #{film["title"]}"
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
+
+
+
+
 
 ## BONUS
 
